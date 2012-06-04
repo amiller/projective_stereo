47eda7545e79cd893847369f5c6b64182aaac95e
Andrew Miller <amiller@dappervision.com>
2012-06-04 16:10:04 -0400

    Moved dividingline into its own project
    Signed-off-by: Andrew Miller <amiller@dappervision.com>

--- demos/demo_dividingline.py
+++
@@ -1,9 +0,0 @@
-from projective_stereo import dividingline; reload(dividingline)
-from projective_stereo.dividingline import DividingLine
-
-line = [1,1,-300]
-im = dividingline.synthetic_image((640,480), line)
-dbg = np.zeros_like(im)
-d = dividingline.DividingLine(im)
-
-res = d._traverse(line, ((0,0),im.shape[::-1]), debug=dbg)
